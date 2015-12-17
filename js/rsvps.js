a5a1c11163181ab5c0d1c1321566768f0ef9dd11
Derek Lindahl <derek.lindahl@gmail.com>
2015-12-17 09:54:32 -0800

    Add better pre- and post-event messaging

--- js/rsvps.js
+++ js/rsvps.js
@@ -1,20 +1,20 @@
 void function(){
   'use strict';
-  var emptyAry = [];
   var upcomingUrl = 'http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=The-Sacramento-Javascript-Meetup&desc=false&offset=0&format=json&page=200&fields=&sig_id=13188390&sig=9041dbac9b59eb70fdc420fb721373c947af147f';
 
   function injectRSVPs(event) {
     var count = event.yes_rsvp_count;
-    emptyAry.forEach.call(
-      document.querySelectorAll('[meetup-id="' + event.id + '"] .RsvpButton--promo'),
-      function(el) {
-        el.innerHTML = 'Join ' + count + ' other' + (count === 1 ? '' : 's');
-      }
-    );
+    var el = document.querySelector('[meetup-id="' + event.id + '"] .RsvpButton--promo');
+    el.innerHTML = 'Join ' + count + ' other' + (count === 1 ? '' : 's');
   }
 
   function parseEvents(response) {
-    response.results.forEach(injectRSVPs);
+    if(response.results.length) {
+      response.results.forEach(injectRSVPs);
+      document.querySelector('.RsvpButton').classList.add('is-visible');
+    } else {
+      document.querySelector('.LeftPane--thanks').classList.add('is-visible');
+    }
   }
 
   function handleXhrError() {
