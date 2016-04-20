10a3ecb0e3d7a87b2873c48691954508970770a2
Derek Lindahl <derek.lindahl@gmail.com>
2016-04-20 09:51:02 -0700

    Add initial NodeBots Day budget

---
+++ test/budgets/2016_nodebots_day_budget.js
@@ -0,0 +1,21 @@
+process.exit(
+     0.00 // starting balance
+
+          // 2016 NodeBots Day Budget
+          // inspired by @brooklynjs
+
++    0.00 // ticket sales         @useTito
++    0.00 // sponsorship          @strongloop
++  619.00 // sponsorship          @sparkfun
+          // 10% off kits
+-  204.79 // NodeBot Event Kit    @sparkfun
+          // https://www.sparkfun.com/wish_lists/126411
+- 6190.00 // attendee hardware    @sparkfun
+          // 40 NodeBot SumoBot Inventor Kits
+          // https://www.sparkfun.com/wish_lists/125260
+-  180.00 // venue rental         @ucdavis
+-  230.98 // breakfast            @noahbgls
+          // ~$109.99 per 20ppl
+-  100.00 // snacks/beverages     @costco
+-  615.20 // lunch                @chipotle
+);
