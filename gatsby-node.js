a6ac6f1f43357831bd68cd43a11f38f2d53f9bc8
Derek <derek.lindahl@gmail.com>
2017-10-09 11:39:19 -0700

    Refactor Markdown node generation
    * Makes `slugGenerator` more generic by extracting out "feature" dupe
    detection and path creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,43 @@ const generateActiveEventPage = require('./src/generators/activeEventGenerator')
 const generateEventPages = require('./src/generators/eventPageGenerator')
 const getEventPages = require('./src/utils/getEventPages')
 
+// Keeps track of the first "featured" event
+let FEATURED_NODE = null
+
+function validateFeatures (node) {
+  if (node.frontmatter.feature) {
+    // If a Featured Event has already been defined, then throw. We cannot have
+    // *two* featured events!
+    if (FEATURED_NODE && FEATURED_NODE.id !== node.id) {
+      throw new Error(
+        `Multiple "featured" events detected: ${FEATURED_NODE.frontmatter
+          .date} and ${node.frontmatter.date}`
+      )
+    }
+    FEATURED_NODE = node
+  }
+  return true
+}
+
+exports.onCreatePage = ({ page, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  page.context = Object.assign({}, page.context, {
+    path: page.context.path || page.path
+  })
+  createPage(page)
+  return Promise.resolve()
+}
+
 exports.onCreateNode = ({ boundActionCreators, node, getNode }) => {
   const { createNodeField } = boundActionCreators
   if (node.internal.type === 'MarkdownRemark') {
-    addSlugToNode(createNodeField, getNode, node)
+    validateFeatures(node)
+    const slug = addSlugToNode(createNodeField, getNode, node)
+    createNodeField({
+      name: 'path',
+      node,
+      value: `/events${slug}`
+    })
   }
 }
 
--- src/generators/slugGenerator.js
+++ src/generators/slugGenerator.js
@@ -1,29 +1,11 @@
 const { createFilePath } = require('gatsby-source-filesystem')
 
-// Keeps track of the first "featured" event
-let FEATURED_NODE = null
-
 module.exports = function addSlugToNode (createNodeField, getNode, node) {
   const slug = createFilePath({ getNode, node })
-  if (node.frontmatter.feature) {
-    // If a Featured Event has already been defined, then throw. We cannot have
-    // *two* featured events!
-    if (FEATURED_NODE && FEATURED_NODE !== node) {
-      throw new Error(
-        `Multiple "featured" events detected: ${FEATURED_NODE.frontmatter
-          .date} and ${node.frontmatter.date}`
-      )
-    }
-    FEATURED_NODE = node
-  }
-  createNodeField({
-    name: 'path',
-    node,
-    value: `/events${slug}`
-  })
   createNodeField({
     name: 'slug',
     node,
     value: slug
   })
+  return slug
 }
