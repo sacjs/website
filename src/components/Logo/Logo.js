4ce7c483f82494d675aa6ab3ce374f4b02186e5c
Derek Lindahl <derek.lindahl@gmail.com>
2017-12-13 12:36:49 -0800

    Replace logo SVG with tinified PNGs and WebPs using the picture element

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,23 @@
-import LogoSvg from '../../../static/img/sac_js.min.svg'
+import LogoPng from '../../../static/img/sac_js@1x.png'
+import LogoRetinaPng from '../../../static/img/sac_js@2x.png'
+import LogoRetinaWebP from '../../../static/img/sac_js@2x.webp'
+import LogoWebP from '../../../static/img/sac_js@1x.webp'
 import React from 'react'
 import { string } from 'prop-types'
 
 const Logo = ({ className }) => (
   <a aria-label="Home" href="/">
-    <img
-      alt=""
-      className={`LogoImg ${className}`}
-      height="150"
-      src={LogoSvg}
-      width="150"
-    />
+    <picture alt="" className={`LogoImg ${className}`} height="150" width="150">
+      <source srcSet={`${LogoRetinaWebP}, ${LogoWebP}`} type="image/webp"/>
+      <source srcSet={`${LogoRetinaPng}, ${LogoPng}`} type="image/png"/>
+      <img
+        alt=""
+        className={`LogoImg ${className}`}
+        height="168"
+        src={LogoPng}
+        width="168"
+      />
+    </picture>
   </a>
 )
 
