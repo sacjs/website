21b22dfa63b45182a06e96d5c06f166755595fcb
Derek <derek.lindahl@gmail.com>
2017-12-16 11:28:32 -0800

    Add extra metadata info for Slack unfurling

--- src/components/Metadata/Event.js
+++ src/components/Metadata/Event.js
@@ -1,11 +1,31 @@
+import dateformat from 'dateformat'
 import Head from 'react-helmet'
-import { object } from 'prop-types'
+import { shape, string } from 'prop-types'
 import React from 'react'
 
-const EventMetadata = ({ event }) => <Head/>
+const EventMetadata = ({ event }) => {
+  const { location, startDate } = event
+  const data1 = dateformat(startDate, 'dddd, mmmm dS, yyyy, h:MM TT')
+  const data2 = `${location.name}, ${location.address.address1}`
+  return (
+    <Head>
+      <meta content="Time" property="twitter:label1"/>
+      <meta content={data1} property="twitter:data1"/>
+      <meta content="Location" property="twitter:label2"/>
+      <meta content={data2} property="twitter:data2"/>
+    </Head>
+  )
+}
 
 EventMetadata.propTypes = {
-  event: object.isRequired
+  event: shape({
+    location: shape({
+      address: shape({
+        address1: string
+      }),
+      name: string
+    })
+  })
 }
 
 export default EventMetadata
