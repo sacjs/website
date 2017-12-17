3e6b6b4a61e0437951f79a18182a70c789828689
Derek <derek.lindahl@gmail.com>
2017-12-17 08:38:36 -0800

    remove more metas

--- src/components/Metadata/Event.js
+++ src/components/Metadata/Event.js
@@ -1,21 +1,19 @@
-import dateformat from 'dateformat'
+// import dateformat from 'dateformat'
 import Head from 'react-helmet'
 import { shape, string } from 'prop-types'
 import React from 'react'
 
-const EventMetadata = ({ event }) => {
-  const { location, startDate } = event
-  const data1 = dateformat(startDate, 'dddd, mmmm dS, yyyy, h:MM TT')
-  const data2 = `${location.name}, ${location.address.address1}`
-  return (
-    <Head>
-      <meta content="Time" property="twitter:label1"/>
-      <meta content={data1} property="twitter:data1"/>
-      <meta content="Location" property="twitter:label2"/>
-      <meta content={data2} property="twitter:data2"/>
-    </Head>
-  )
-}
+const EventMetadata = ({ event }) => (
+  // const { location, startDate } = event
+  // const data1 = dateformat(startDate, 'dddd, mmmm dS, yyyy, h:MM TT')
+  // const data2 = `${location.name}, ${location.address.address1}`
+  <Head>
+    {/* <meta content="Time" property="twitter:label1"/> */}
+    {/* <meta content={data1} property="twitter:data1"/> */}
+    {/* <meta content="Location" property="twitter:label2"/> */}
+    {/* <meta content={data2} property="twitter:data2"/> */}
+  </Head>
+)
 
 EventMetadata.propTypes = {
   event: shape({
