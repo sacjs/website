145088ccd16b53111b408054765172e0420c1992
Derek <derek.lindahl@gmail.com>
2017-12-17 08:44:40 -0800

    try object type

--- src/components/Metadata/Site.js
+++ src/components/Metadata/Site.js
@@ -39,27 +39,28 @@ const SiteMetadata = ({
   }
   return (
     <Head>
-      {/* <meta content={siteName} name="apple-mobile-web-app-title"/>
-      <meta content={siteName} name="application-name"/>
-      <link
+      {/* <meta content={siteName} name="apple-mobile-web-app-title"/> */}
+      {/* <meta content={siteName} name="application-name"/> */}
+      {/* <link
         href="/img/favicons/apple-touch-icon.png"
         rel="apple-touch-icon"
         sizes="180x180"
-      />
-      <link href="/img/favicons/favicon-16x16.png" rel="shortcut icon"/>
-      <link
+      /> */}
+      {/* <link href="/img/favicons/favicon-16x16.png" rel="shortcut icon"/> */}
+      {/* <link
         color={themeColor}
         href="/img/favicons/safari-pinned-tab.svg"
         rel="mask-icon"
-      />
-      <meta content={siteName} property="og:site_name"/>
-      <meta content={title} property="og:title"/>
-      <meta content="website" property="og:type"/>
-      <meta content={absoluteUrl(slug)} property="og:url"/> */}
+      /> */}
+      {/* <meta content={siteName} property="og:site_name"/> */}
+      {/* <meta content={title} property="og:title"/> */}
+      {/* <meta content="website" property="og:type"/> */}
+      {/* <meta content={absoluteUrl(slug)} property="og:url"/> */}
       <meta
         content="https://avatars1.githubusercontent.com/u/6962987?s=400&amp;v=4"
         property="og:image"
       />
+      <meta content="object" property="og:type"/>
       {/* <meta content={absoluteUrl(logo.url)} property="og:image"/> */}
       {/* <meta content={logo.width} property="og:image:width"/> */}
       {/* <meta content={logo.height} property="og:image:height"/> */}
